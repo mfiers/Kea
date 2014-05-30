70d6465ddd9c387284b1106954d9abbfd9aaf710
Mark Fiers <mark.fiers.42@gmail.com>
2014-05-30 16:30:36 +0200

    warn when log file cannot be created instead of crash

--- kea/plugin/logger.py
+++ kea/plugin/logger.py
@@ -1,11 +1,13 @@
 
 from collections import OrderedDict
+import logging
 
-from mad2 import madfile
 import arrow
-from lockfile import FileLock
+from mad2 import madfile
 import leip
+from lockfile import FileLock
 
+lg = logging.getLogger(__name__)
 
 def to_str(s):
     if isinstance(s, madfile.MadFile):
@@ -13,23 +15,27 @@ def to_str(s):
             return '{} (sha1: {})'.format(s['inputfile'], s['sha1sum'])
         else:
             return '{}'.format(s['inputfile'])
-
     else:
         return str(s)
 
 @leip.hook('post_run')
 def log_cl(app, all_info):
 
-    with FileLock('kea.log'):
-        for i, info in enumerate(all_info):
-            with open('kea.log', 'a') as F:
-                F.write("-" * 80 + "\n")
-                for i in info:
-                    F.write("{}: ".format(i))
-                    val = info[i]
-                    if isinstance(val, list):
-                        F.write("\n")
-                        for lv in val:
-                            F.write(' - {}\n'.format(to_str(lv)))
-                    else:
-                        F.write(" {}\n".format(to_str(val)))
+    try:
+        with FileLock('kea.log'):
+            for i, info in enumerate(all_info):
+                with open('kea.log', 'a') as F:
+                    F.write("-" * 80 + "\n")
+                    for i in info:
+                        F.write("{}: ".format(i))
+                        val = info[i]
+                        if i == 'cl':
+                            F.write(" ".join(val) + "\n")
+                        elif isinstance(val, list):
+                            F.write("\n")
+                            for lv in val:
+                                F.write(' - {}\n'.format(to_str(lv)))
+                            else:
+                                F.write(" {}\n".format(to_str(val)))
+    except:
+        lg.warning("Cannot write to log file")
