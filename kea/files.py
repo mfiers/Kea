ae0d52343392113900cb8d24ec77f2da6dd528f6
Mark Fiers <mark.fiers.42@gmail.com>
2014-07-15 13:21:58 +0200

    better in/outputfile recognition

--- kea/files.py
+++ kea/files.py
@@ -1,22 +1,37 @@
 
+import logging
+
+from mad2.recrender import recrender
 
 import kea.mad
 
-def set_input(info, filename):
+
+lg = logging.getLogger(__name__)
+
+
+def register_file(info, name, category, filename):
+    lg.debug("set %s as %s file: '%s'", filename, category, name)
     madfile = kea.mad.get_madfile(filename)
-    name = 'input'
+
     info['files'][name]['filename'] = filename
     info['files'][name]['madfile'] = madfile
-    info['files'][name]['category'] = 'input'
+    info['files'][name]['category'] = category
+
+
+def set_input(info, filename):
+    lg.debug("set %s as input file", filename)
+    madfile = kea.mad.get_madfile(filename)
+    info['files']['input']['filename'] = filename
+    info['files']['input']['madfile'] = madfile
+    info['files']['input']['category'] = 'input'
 
 
 def set_output(info, filename):
+    lg.debug("set %s as output file", filename)
     madfile = kea.mad.get_madfile(filename)
-    name = 'output'
-    print filename
-    info['files'][name]['filename'] = filename
-    info['files'][name]['madfile'] = madfile
-    info['files'][name]['category'] = 'output'
+    info['files']['output']['filename'] = filename
+    info['files']['output']['madfile'] = madfile
+    info['files']['output']['category'] = 'output'
 
 
 def assign_on_position(info, name, category, pos):
@@ -24,6 +39,7 @@ def assign_on_position(info, name, category, pos):
     madfile = kea.mad.get_madfile(filename)
     info['files'][name]['madfile'] = madfile
     info['files'][name]['category'] = category
+
 
 def assign_filename(info, name, category, filename):
     madfile = kea.mad.get_madfile(filename)
