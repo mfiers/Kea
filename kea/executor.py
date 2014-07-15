15668dc440b6a8b899fe3655811ea7e022db8828
Mark Fiers <mark.fiers.42@gmail.com>
2014-07-15 13:20:56 +0200

    basic support for ctrl-c capturing

--- kea/executor.py
+++ kea/executor.py
@@ -3,13 +3,13 @@ import copy
 import logging
 from multiprocessing.dummy import Pool as ThreadPool
 import os
+import signal
 import subprocess as sp
 import sys
 
 import arrow
 
 lg = logging.getLogger(__name__)
-
 
 
 def get_deferred_cl(info):
@@ -21,7 +21,6 @@ def get_deferred_cl(info):
     if info['stderr_file']:
         cl.extend(['{}-e'.format(kap), info['stderr_file']])
     return cl
-
 
 
 def simple_runner(info, defer_run=False):
@@ -54,20 +53,30 @@ def simple_runner(info, defer_run=False):
         P = sp.Popen(cl)
         info['pid'] = P.pid
         info['submitted'] = arrow.now()
+        info['status'] = 'deferred'
     else:
-        P = sp.Popen(cl, stdout=stdout_handle, stderr=stderr_handle)
+
+        def preexec(): # Don't forward signals.
+            os.setpgrp()
+
+        P = sp.Popen(cl, stdout=stdout_handle, stderr=stderr_handle,
+                     preexec_fn = preexec)
         info['pid'] = P.pid
         P.communicate()
         info['stop'] = arrow.now()
         info['stop'] = arrow.now()
         info['runtime'] = info['stop'] - info['start']
         info['returncode'] = P.returncode
-
+        if info['returncode'] == 0:
+            info['status'] = 'success'
+        else:
+            info['status'] = 'error'
 
 class BasicExecutor(object):
 
     def __init__(self, app):
         lg.debug("Starting executor")
+        self.interrupted = False
         self.app = app
         self.threads =  self.app.kea_args.threads
         if self.threads < 2:
@@ -81,17 +90,49 @@ class BasicExecutor(object):
     def fire(self, info):
         lg.debug("start execution")
 
+        if self.interrupted:
+            #refuse execution after an interrupt was caught
+            info['returncode'] = 2
+            info['status'] = 'not_executed'
+            return
+
+        def sigint_handler(sgn, frame):
+            #capture sigint
+            #send sigint
+            if self.interrupted:
+                lg.warning('Captured Ctrl+C twice - exit now')
+                sys.exit(-1)
+
+            if self.simple:
+                self.interrupted = True
+                info['status'] = 'interrupted'
+                lg.warning('Captured Ctrl+C - quitting')
+                lg.warning('Sending SIGINT to %d', info['pid'])
+                os.kill(info['pid'], signal.SIGINT)
+                #os.kill(info['pid'], signal.SIGKILL)
+
+
+        # weirdly enough, this following line makes the less pager
+        # behave normally - i.e. this program quits when less quits
+        signal.signal(signal.SIGPIPE, lambda s,f: None)
+
+        # capture sigint as well
+        signal.signal(signal.SIGINT, sigint_handler)
+
         if self.simple:
             simple_runner(info)
         else:
             self.pool.apply_async(simple_runner, [info,], {'defer_run': False})
 
+
     def finish(self):
         if not self.simple:
-            lg.warning('waiting for the threads to finish')
+            lg.debug('waiting for the threads to finish')
             self.pool.close()
             self.pool.join()
-            lg.warning('finished waiting for threads to finish')
+            lg.debug('finished waiting for threads to finish')
+
+        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 
 class DummyExecutor(BasicExecutor):
