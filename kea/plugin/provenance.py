16fcb28568f6b97701e6ac74835855e394bb9624
Mark Fiers <mark.fiers.42@gmail.com>
2014-08-06 16:16:37 +0200

    much improvement

--- kea/plugin/provenance.py
+++ kea/plugin/provenance.py
@@ -15,6 +15,7 @@ import kea.mad
 lg = logging.getLogger(__name__)
 #lg.setLevel(logging.DEBUG)
 
+
 def get_last_provenance_data(madfile):
     if not 'provenance' in madfile:
         return None
@@ -50,43 +51,75 @@ def check_output(app, info):
 @leip.hook('pre_fire')
 def check_run(app, info):
 
-    all_output_ok = True
+    lg.debug("Check if we need to run this command")
+
+    no_files_notok = 0
     no_outputfiles_seen = 0
 
+    output_files = {}
+    all_filenames = set()
+
+    #first, find all outputfiles
+    #also - if an outputfile does not exists - decide to run right away
+    #also - if an inputfile is not present - decide to run also
+    #       it is not up to Kea to decide not to run in the case of an
+    #       error - the tool may complain
     for name in info['files']:
         finf = info['files'][name]
-        if finf['category'] != 'output':
-            if not os.path.exists(finf['filename']):
-                lg.warning("Cannot find %s file %s",
-                           finf['category'], finf['filename'])
-                exit(-1)
-        else:
-            no_outputfiles_seen += 1
+        all_filenames.add(name)
 
-            if not os.path.exists(finf['filename']):
+        if not os.path.exists(finf['filename']):
+            if finf['category'] == 'output':
                 #output file does not exist - continue
+                lg.debug("Output file %s does not exist - run", name)
+                return
+            else:
+                #non output file - should have been present
+                lg.warning("Cannot check provenance")
+                lw.warning("%s/%s file: %s does not exist",
+                           finf['category'], name, finf['filename'])
                 return
 
-            #find output file - check if inputs have changed
-            lg.debug("check output file %s", finf['filename'])
-            prod = get_last_provenance_data(finf['madfile'])
-            if not prod:
-                #no provenance data for this outputfile - so - run
-                return
-            for fn in prod.get('derived_from', []):
-                fnd = prod['derived_from'][fn]
-                kea_name = fnd.get('kea_name', '')
-                this_run_finf = info['files'].get(kea_name)
-                current_sha1 = this_run_finf['madfile']['sha1sum']
-                # print('derivedfrom ', fnd)
-                # print('currentsha  ', current_sha1)
-                # print('thisrunfinf ', this_run_finf)
-                if fnd['sha1sum'] != current_sha1:
-                    all_output_ok = False
+        if finf['category'] == 'output':
+            lg.debug("found output: %s", name)
+            no_outputfiles_seen += 1
+            output_files[name] = finf
 
-    if no_outputfiles_seen > 1 and  all_output_ok:
-        lg.warning("Skipping - no input files have changed")
-        exit(0)
+    for output_name, output_finf in output_files.items():
+
+        #check if the input/outputfile structure is consistent
+
+        #find output file - check if inputs have changed
+        lg.debug("check provenance of: %s", finf['filename'])
+        out_prov = get_last_provenance_data(output_finf['madfile'])
+
+        if not out_prov:
+            lg.debug("no provenance data for output file %s", output_name)
+            #no provenance data for this outputfile - so - run
+            return
+
+        if set(out_prov['derived_from'].keys()) != all_filenames:
+            #provenance recorded filenames do not match this run's filenames
+            #hence - we shall run the tool:
+            lg.debug("provenance data of %s does not match current run",
+                     output_name)
+            return
+
+        #print(out_prov.pretty())
+        for fn in out_prov.get('derived_from', []):
+            lg.debug(" - prov check %s", fn)
+            out_prov_file = out_prov['derived_from'][fn]
+            fn_finf = info['files'].get(fn)
+            current_sha1 = fn_finf['madfile']['sha1sum']
+            lg.debug(' - current sha1       : %s', current_sha1)
+            lg.debug(' - prov recorded sha1 : %s', out_prov_file['sha1sum'])
+            if out_prov_file['sha1sum'] != current_sha1:
+                lg.debug(" - sha1sum mismatch!")
+                no_files_notok += 1
+
+    if no_outputfiles_seen > 0 and  no_files_notok == 0:
+        lg.warning("Skipping - provenance data indicates that this is a rerun")
+        info['skip'] = True
 
 
 def annotate(app, info, fname, fdata):
@@ -94,8 +127,11 @@ def annotate(app, info, fname, fdata):
 
     lg.debug("annotating '%s' output file: '%s'",
              fname, fdata['filename'])
+
     maf = fdata['madfile']
     maf.load() # make sure we're dealing with current data
+
+
     stamp = str(info['stop']).split('.')[0]
     prod = maf.mad['provenance.%s' % stamp]
     prod['tool_name'] = info['app_name']
@@ -117,13 +153,12 @@ def annotate(app, info, fname, fdata):
     for fn in info['files']:
         fd = info['files'][fn]
         fdmaf = fd['madfile']
-        if fd['category'] == 'output':
-            continue
-        if fd['category'] == 'input':
-            derf = prod['derived_from'][fn]
-        elif fd['category'] == 'used':
-            derf = prod['used'][fn]
+
+        #store all recorded files
+        derf = prod['derived_from'][fn]
+
         derf['filename'] = fdmaf['fullpath']
+        derf['category'] = fd['category']
         derf['kea_name'] = fn
         derf['sha1sum'] = fdmaf['sha1sum']
         derf['host'] = fdmaf['host']
