6dbddea930505711734236e7938cb080fc4146be
Mark Fiers <mark.fiers.42@gmail.com>
2015-01-28 15:02:31 +0100

    initial transaction support

---
+++ kea/plugin/transaction.py
@@ -0,0 +1,299 @@
+from __future__ import print_function
+
+import copy
+from datetime import datetime
+from collections import defaultdict
+import gzip
+import hashlib
+from itertools import chain
+import logging
+import os
+
+from bson.objectid import ObjectId
+import gnupg
+import leip
+from termcolor import cprint
+import yaml
+
+from kea.plugin.logger import dictprint
+import kea.utils
+
+lg = logging.getLogger(__name__)
+
+MADAPP = None       # contains mad applicaton object
+
+
+@leip.hook('pre_argparse')
+def transaction_arg_define(app):
+    tragroup = app.parser.add_argument_group('Transaction plugin')
+    tragroup.add_argument('--st', dest = 'save_transation_to_disk', help='save transaction to disk',
+                          action='store_true')
+    tragroup.add_argument('-B', '--always_run', help='do not skip because of transaction match',
+                          action='store_true')
+
+
+def hash(o):
+    o = str(o)
+    h = hashlib.sha256()
+    h.update(o)
+    return h.hexdigest()
+
+
+# thanks: https://stackoverflow.com/questions/5884066/hashing-a-python-dictionary
+def make_hash(o):
+  """
+  Makes a hash from a dictionary, list, tuple or set to any level, that contains
+  only other hashable types (including any lists, tuples, sets, and
+  dictionaries).
+  """
+
+  if isinstance(o, (set, tuple, list)):
+    return tuple([make_hash(e) for e in o])
+
+  elif not isinstance(o, dict):
+    return hash(o)
+
+  new_o = copy.deepcopy(o)
+  for k, v in new_o.items():
+    new_o[k] = make_hash(v)
+
+  return hash(tuple(frozenset(sorted(new_o.items()))))
+
+def get_coll_transaction(conf):
+    mconf = conf['plugin.transaction']
+
+    c2t_name = 'checksum2transaction'
+    tra_name = 'transaction'
+
+    c2t = kea.utils.get_mongo_collection(conf, c2t_name)
+
+    tra = kea.utils.get_mongo_collection(conf, tra_name)
+
+    tra.ensure_index('timestamp')
+    tra.ensure_index('transaction_id')
+
+    c2t.ensure_index('application')
+    c2t.ensure_index('timestamp')
+    c2t.ensure_index('category')
+    c2t.ensure_index('application')
+    c2t.ensure_index('file_sha1sum')
+    c2t.ensure_index('category')
+
+    return tra, c2t
+
+
+def get_madapp():
+    global MADAPP
+    if MADAPP is None:
+        MADAPP = leip.app(name='mad2', disable_commands=True)
+    return MADAPP
+
+
+@leip.hook('post_fire')
+def mad_register_file(app, jinf):
+    from mad2.util import get_mad_file
+
+    madapp = get_madapp()
+
+    for cat in ['input', 'output', 'database', 'use']:
+        for name, fdata in jinf.get(cat, {}).items():
+            filename = fdata['path']
+            madfile = get_mad_file(madapp, filename)
+            try:
+                fdata['sha1sum'] = str(madfile['sha1sum'])
+            except:
+                lg.warning("error getting mad/sha1sum for %s", filename)
+                continue
+
+
+
+@leip.hook('pre_fire', 1000)
+def check_transaction(app, jinf):
+    lg.debug("check transaction")
+
+    madfiles = {}
+    mng_tra, mng_c2t = get_coll_transaction(app.conf)
+
+    from mad2.util import get_mad_file
+
+    if not 'output' in jinf:
+        #no outputfiles - can't really check if we need to rerun
+        return
+
+    madapp = get_madapp()
+
+    all_outputfiles_exist = True
+    #check if all output files exist
+    for outfile, outfile_data in jinf.get('output', {}).items():
+        filename = outfile_data['path']
+        if not os.path.exists(filename):
+            all_outputfiles_exist = False
+        else:
+            madfile = get_mad_file(madapp, filename)
+            madfiles[filename] = madfile
+
+    if not all_outputfiles_exist:
+        return
+
+    transact_list = defaultdict(lambda: set())
+
+    for infile, infile_data in chain(jinf.get('input', {}).items(),
+                                     jinf.get('database', {}).items(),
+                                     jinf.get('use', {}).items()):
+        filename = infile_data['path']
+        madfile = get_mad_file(madapp, filename)
+        query = {'name': infile,
+                 'application': app.name,
+                 'file_sha1sum': madfile['sha1sum']}
+        for rec in mng_c2t.find(query):
+            transact_list[infile].add(rec['transaction_id'])
+
+
+    translist = transact_list.values()[0]
+    for t in transact_list:
+        translist &= transact_list[t]
+
+    if len(translist) == 0:
+        lg.debug("no candidate transaction found based on input file sha1sums")
+        return
+
+#    lg.setLevel(logging.DEBUG)
+    lg.debug("Found %d %s transactions with the same inputfiles", len(translist), app.name)
+
+    one_transaction_matches = False
+
+    for traid in translist:
+        lg.debug("checking transaction %s", traid)
+        tra = mng_tra.find_one({'transaction_id': traid})
+        trarec = tra['transaction']
+
+#        dictprint(tra)
+        output_matches = True
+        #check if output files match
+        for outfile, outfile_data in jinf.get('output', {}).items():
+            filename = outfile_data['path']
+            if not os.path.exists(filename):
+                output_matches = False
+                break
+
+            if not 'output' in trarec:
+                output_matches = False
+                break
+
+            if madfiles[filename]['sha1sum'] != trarec['output'][outfile]['sha1sum']:
+                output_matches = False
+                break
+            lg.debug("match %s (%s)", filename, madfiles[filename]['sha1sum'])
+
+        if not output_matches:
+            continue
+
+        lg.info('transaction match!')
+        one_transaction_matches = True
+
+    if one_transaction_matches and (not app.args.always_run):
+        lg.warning("Transaction match -> Skipping job")
+        jinf['skip'] = True
+
+
+@leip.hook('post_fire', 1000)
+def create_transaction(app, jinf):
+    #clean jinf a little
+    dat = yaml.load(yaml.safe_dump(dict(jinf), default_flow_style=False))
+
+    if not jinf['status'] == 'success':
+        lg.warning("Not storing transaction (status:%s)", jinf['status'])
+        return
+
+    tra = {}
+    tra['transaction'] = dat
+    tra['timestamp'] = datetime.utcnow().isoformat()
+    tra['transaction_id'] = make_hash(dat)
+
+    #attempt signature
+    # gpg = gnupg.GPG(use_agent=True)
+    # key = gpg.list_keys()[0]
+    # tra['signature'] = str(gpg.sign(tra['sha256'], detach=True))
+    # tra['signature_id'] = key['uids'][0]
+    # tra['signature_key'] = key['keyid']
+    #print(gpg)
+
+    if app.args.save_transation_to_disk:
+        filename = "{}.{}.{}.transaction".format(
+            app.name, jinf['run']['uid'], jinf['run']['no'])
+        with gzip.open(filename, 'w') as F:
+            F.write(yaml.safe_dump(tra, default_flow_style=False))
+
+    mc_tra, mc_c2t = get_coll_transaction(app.conf)
+
+    tra_recid = str(mc_tra.insert(tra))
+
+    for cat in ['input', 'output', 'database', 'use']:
+        if not cat in dat:
+            continue
+        for fname in dat[cat]:
+            fdat = dat[cat][fname]
+            rec = dict(transaction_record_id = tra_recid,
+                       transaction_id = tra['transaction_id'],
+                       category = cat,
+                       timestamp = datetime.utcnow(),
+                       application = app.name,
+                       name = fname,
+                       filename = os.path.basename(fdat['path']),
+                       file_sha1sum = fdat['sha1sum'])
+            mc_c2t.insert(rec)
+
+
+
+@leip.subparser
+def tra(app, args):
+    pass
+
+@leip.arg("file")
+@leip.subcommand(tra, "find")
+def find_transactions(app, args):
+    from mad2.util import get_mad_file
+
+    filename = args.file
+    madapp = get_madapp()
+    madfile = get_mad_file(madapp, filename)
+    sha1sum = madfile['sha1sum']
+
+    mongo_tra, mongo_c2t = get_coll_transaction(app.conf)
+
+    for rec in mongo_c2t.find({'file_sha1sum': sha1sum}):
+        tract = mongo_tra.find_one(
+            {'_id': ObjectId(rec['transaction_record_id'])})
+        jinf = tract['transaction']
+
+        cprint("# TRANSACTION: ", end="")
+        cprint(tract['transaction_id'], "green")
+        for cat in ['input', 'database', 'use', 'output']:
+            if not cat in jinf:
+                continue
+            cprint("Category: ", end="")
+            cprint("{:10}".format(cat), "yellow")
+            for fk, fd in sorted(jinf[cat].items()):
+                cprint(" - {}".format(fk), "blue", end=":")
+                fdp = fd['path']
+                s1s = fd['sha1sum']
+                if s1s == sha1sum:
+                    cprint("*", "yellow", end=":")
+                if os.path.exists(fdp):
+                    cprint(fd['path'], "green")
+                else:
+                    cprint(fd['path'], "red")
+
+
+@leip.arg('transaction_file')
+@leip.subcommand(tra, 'validate')
+def validate_transaction(app, args):
+    trafile= args.transaction_file
+    with gzip.open(trafile) as F:
+        tra = yaml.load(F)
+    hatra = make_hash(tra['transaction'])
+
+    if hatra == tra['transaction_id']:
+        print("{} ok".format(trafile))
+    else:
+        print("{} FAIL".format(trafile))
